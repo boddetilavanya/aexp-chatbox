5407f0278e5da5762397d6379fd169afd9bb5f4e
Boddeti <lavanya.boddeti@capgemini.com>
2019-01-30 22:33:56 -0800

    Initial commit

---
+++ src/main/java/com/aexp/aechatbox/AeChatBoxApplication.java
@@ -0,0 +1,14 @@
+package com.aexp.aechatbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeChatBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeChatBoxApplication.class, args);
+	}
+
+}
+
